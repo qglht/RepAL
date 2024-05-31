a5372928fbbb76dd75577fa83fb10a6fb811c3fc
qglht <quentin.guilhot@student-cs.fr>
2024-05-31 12:36:10 +0100

    tasks

--- src/test.py
+++ src/test.py
@@ -32,52 +32,54 @@ if __name__ == "__main__":
 
     i = 0  # Index to cycle through available devices
 
-    curves_frozen = []
-    curves_frozen_names = []
-    curves_unfrozen = []
-    curves_unfrozen_names = []
-    explained_variances_frozen = []
-    explained_variances_unfrozen = []
-    dissimilarities = {"within_unfrozen": {}, "within_frozen": {}, "across": []}
-    for activation in config["rnn"]["parameters"]["activations"]:
-        for hidden_size in config["rnn"]["parameters"]["n_rnn"]:
-            for lr in config["rnn"]["parameters"]["learning_rate"]:
-                for freeze in config["rnn"]["parameters"]["freeze"]:
-                    # curve, explained_variance = compute_dissimilarity(
-                    #     activation, hidden_size, lr, freeze, "cpu"
-                    # )
-                    device = devices[i % len(devices)]  # Cycle through available devices
-                    train_model(activation, hidden_size, lr, freeze, "pretrain", True, "cpu")
-                    # if freeze:
-                    #     curves_frozen.append(curve)
-                    #     curves_frozen_names.append(
-                    #         f"{activation}_{hidden_size}_{lr}"
-                    #     )
-                    #     explained_variances_frozen.append(explained_variance)
-                    # else:
-                    #     curves_unfrozen.append(curve)
-                    #     curves_unfrozen_names.append(
-                    #         f"{activation}_{hidden_size}_{lr}"
-                    #     )
-                    #     explained_variances_unfrozen.append(explained_variance)
+    device = devices[0]
+    train_model("softplus", 128, 0.001, False, "train", False, device)
+    # curves_frozen = []
+    # curves_frozen_names = []
+    # curves_unfrozen = []
+    # curves_unfrozen_names = []
+    # explained_variances_frozen = []
+    # explained_variances_unfrozen = []
+    # dissimilarities = {"within_unfrozen": {}, "within_frozen": {}, "across": []}
+    # for activation in config["rnn"]["parameters"]["activations"]:
+    #     for hidden_size in config["rnn"]["parameters"]["n_rnn"]:
+    #         for lr in config["rnn"]["parameters"]["learning_rate"]:
+    #             for freeze in config["rnn"]["parameters"]["freeze"]:
+    #                 # curve, explained_variance = compute_dissimilarity(
+    #                 #     activation, hidden_size, lr, freeze, "cpu"
+    #                 # )
+    #                 device = devices[i % len(devices)]  # Cycle through available devices
+    #                 train_model(activation, hidden_size, lr, freeze, "pretrain", True, "cpu")
+    #                 # if freeze:
+    #                 #     curves_frozen.append(curve)
+    #                 #     curves_frozen_names.append(
+    #                 #         f"{activation}_{hidden_size}_{lr}"
+    #                 #     )
+    #                 #     explained_variances_frozen.append(explained_variance)
+    #                 # else:
+    #                 #     curves_unfrozen.append(curve)
+    #                 #     curves_unfrozen_names.append(
+    #                 #         f"{activation}_{hidden_size}_{lr}"
+    #                 #     )
+    #                 #     explained_variances_unfrozen.append(explained_variance)
 
-    # create a dataset from main.dataset import Dataset, get_class_instance
+    # # create a dataset from main.dataset import Dataset, get_class_instance
 
-    # config = load_config("config.yaml")
-    # ruleset = config["rnn"]["train"]["ruleset"]
-    # all_rules = config["rnn"]["train"]["ruleset"] + config["rnn"]["pretrain"]["ruleset"]
-    # hp = {
-    #     "activation": "softplus",
-    #     "n_rnn": 128,
-    #     "learning_rate": 0.001,
-    #     "l2_h": 0.000001,
-    #     "l2_weight": 0.000001,
-    # }
-    # hp, log, optimizer = main.set_hyperparameters(
-    #     model_dir="debug", hp=hp, ruleset=all_rules, rule_trains=ruleset
-    # )
-    # env = get_class_instance("PerceptualDecisionMakingT", config=hp)
-    # dataset = Dataset(env, batch_size=100, seq_len=100)
-    # inputs, labels = dataset.dataset()
-    # ipdb.set_trace()
-    # dataset.plot_env()
+    # # config = load_config("config.yaml")
+    # # ruleset = config["rnn"]["train"]["ruleset"]
+    # # all_rules = config["rnn"]["train"]["ruleset"] + config["rnn"]["pretrain"]["ruleset"]
+    # # hp = {
+    # #     "activation": "softplus",
+    # #     "n_rnn": 128,
+    # #     "learning_rate": 0.001,
+    # #     "l2_h": 0.000001,
+    # #     "l2_weight": 0.000001,
+    # # }
+    # # hp, log, optimizer = main.set_hyperparameters(
+    # #     model_dir="debug", hp=hp, ruleset=all_rules, rule_trains=ruleset
+    # # )
+    # # env = get_class_instance("PerceptualDecisionMakingT", config=hp)
+    # # dataset = Dataset(env, batch_size=100, seq_len=100)
+    # # inputs, labels = dataset.dataset()
+    # # ipdb.set_trace()
+    # # dataset.plot_env()
