944b306b8d73a4b9612b868d361f36405a2fe9fd
qglht <quentin.guilhot@student-cs.fr>
2024-06-17 13:47:10 +0100

    train_group

--- src/train.py
+++ src/train.py
@@ -1,60 +1,57 @@
-import warnings
+import numpy as np
 import os
-
+import sys
+from subprocess import call
+import warnings
+import argparse
 from dsa_analysis import load_config
 import torch
 import multiprocessing
 from src.toolkit import pipeline
+from src.train_group import train
 
-# Suppress specific Gym warnings
-warnings.filterwarnings("ignore", message=".*Gym version v0.24.1.*")
-warnings.filterwarnings("ignore", message=".*The `registry.all` method is deprecated.*")
+def generate_and_submit_scripts():
+    config = load_config("config.yaml")
+    groups = config['groups']  # Assuming 'groups' is a key in your YAML file
 
-# Set environment variable to ignore Gym deprecation warnings
-os.environ['GYM_IGNORE_DEPRECATION_WARNINGS'] = '1'
+    script_template = """
+                        #!/bin/bash
+
+                        #SBATCH --nodes=1
+                        #SBATCH --time=24:00:00
+                        #SBATCH --job-name={group}_job
+                        #SBATCH --gres=gpu:8
+                        #SBATCH --mail-type=ALL
+                        #SBATCH --mail-user=oxfd2547@ox.ac.uk
+
+                        module load cuda/11.2
+                        module load pytorch/1.9.0
+                        module load python/anaconda3
+
+                        source activate dsa
+                        poetry install
+
+                        nvidia-smi --query-gpu=timestamp,name,utilization.gpu,utilization.memory --format=csv,nounits -l 300 > {group}_gpu_usage.log &
+                        
+                        MONITOR_PID=$!
+
+                        poetry run python src.train_group --group {group}
+
+                        kill $MONITOR_PID
+                    """
+
+    for group in groups:
+        script_content = script_template.format(group=group)
+        script_filename = f"sbatch/{group}_script.sh"
+
+        with open(script_filename, 'w') as script_file:
+            script_file.write(script_content)
+
+        # Submit the job to the cluster
+        call(f"sbatch {script_filename}", shell=True)
 
 if __name__ == "__main__":
-    multiprocessing.set_start_method("spawn", force=True)
-    config = load_config("config.yaml")
+    generate_and_submit_scripts()
 
-    # Create a list of all tasks to run
-    tasks = []
-    num_gpus = torch.cuda.device_count()  # Get the number of GPUs available
-    devices = (
-        [torch.device(f"cuda:{i}") for i in range(num_gpus)]
-        if num_gpus > 0
-        else [torch.device("cpu")]
-    )
-    i = 0
-    print(f"devices used : {devices}")
-    print(f"number of devices : {num_gpus}")
 
-    # create a folder for each group in config['groups'] under model folder
-    for group in config["groups"]:
-        if not os.path.exists(f"models/{group}"):
-            os.makedirs(f"models/{group}")
 
-    for group in config["groups"]:
-        for rnn_type in config["rnn"]["parameters"]["rnn_type"]:
-            for activation in config["rnn"]["parameters"]["activations"]:
-                for hidden_size in config["rnn"]["parameters"]["n_rnn"]:
-                    for lr in config["rnn"]["parameters"]["learning_rate"]:
-                        for batch_size in config["rnn"]["parameters"]["batch_size_train"]:
-                            device = devices[
-                                i % len(devices)
-                            ]  # Cycle through available devices
-                            tasks.append((group, rnn_type, activation, hidden_size, lr, batch_size, device))
-                            i += 1
-
-    # Create a process for each task
-    processes = [
-        multiprocessing.Process(target=pipeline, args=task) for task in tasks
-    ]
-
-    # Start all processes
-    for process in processes:
-        process.start()
-
-    # Wait for all processes to finish
-    for process in processes:
-        process.join()
