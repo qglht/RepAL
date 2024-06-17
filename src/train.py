13ca955f827ea43cc9fbda3d23323f2f863cea1a
qglht <quentin.guilhot@student-cs.fr>
2024-06-17 14:10:04 +0100

    train

--- src/train.py
+++ src/train.py
@@ -14,31 +14,29 @@ def generate_and_submit_scripts():
     config = load_config("config.yaml")
     groups = config['groups']  # Assuming 'groups' is a key in your YAML file
 
-    script_template = """
-                        #!/bin/bash
+    script_template = """#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name={group}_job
+#SBATCH --gres=gpu:8
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=oxfd2547@ox.ac.uk
 
-                        #SBATCH --nodes=1
-                        #SBATCH --time=24:00:00
-                        #SBATCH --job-name={group}_job
-                        #SBATCH --gres=gpu:8
-                        #SBATCH --mail-type=ALL
-                        #SBATCH --mail-user=oxfd2547@ox.ac.uk
+module load cuda/11.2
+module load pytorch/1.9.0
+module load python/anaconda3
 
-                        module load cuda/11.2
-                        module load pytorch/1.9.0
-                        module load python/anaconda3
+source activate dsa
+poetry install
 
-                        source activate dsa
-                        poetry install
+nvidia-smi --query-gpu=timestamp,name,utilization.gpu,utilization.memory --format=csv,nounits -l 300 > {group}_gpu_usage.log &
 
-                        nvidia-smi --query-gpu=timestamp,name,utilization.gpu,utilization.memory --format=csv,nounits -l 300 > {group}_gpu_usage.log &
-                        
-                        MONITOR_PID=$!
+MONITOR_PID=$!
 
-                        poetry run python src.train_group --group {group}
+poetry run python src.train_group --group {group}
 
-                        kill $MONITOR_PID
-                    """
+kill $MONITOR_PID
+"""
 
     for group in groups:
         script_content = script_template.format(group=group)
