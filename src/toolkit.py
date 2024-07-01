8fd9f94bb5ef95d654248a78cc1a706961286f4a
qglht <quentin.guilhot@student-cs.fr>
2024-07-01 16:08:17 +0100

    fix

--- src/toolkit.py
+++ src/toolkit.py
@@ -410,7 +410,7 @@ def dissimilarity_within_learning(
                     map_location=device,
                 )
                 run_model.load_state_dict(checkpoint["model_state_dict"])
-                accuracy = float(checkpoint["log"]["perf_min"])
+                accuracy = float(checkpoint["log"]["perf_min"][-1])
                 # convert accuracy to float if it was a string
                 models_to_compare.extend([run_model])
                 accuracies.append(accuracy)
@@ -423,6 +423,7 @@ def dissimilarity_within_learning(
 
             groups = []
             print(f"accuracies : {accuracies}")
+            ipdb.set_trace()
             for i in range(len(sampling) - 1):
                 print(f"Computing group {i}")
                 print(f"len curves : {len(curves)}")
