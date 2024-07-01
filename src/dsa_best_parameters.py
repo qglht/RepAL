3b47318e34ac4d838fa705e2937d405adb2969c0
qglht <quentin.guilhot@student-cs.fr>
2024-07-01 12:15:09 +0100

    fix

--- src/dsa_best_parameters.py
+++ src/dsa_best_parameters.py
@@ -9,6 +9,7 @@ import torch
 import multiprocessing
 from src.toolkit import pipeline
 
+
 def generate_and_submit_scripts():
     script_template = """#!/bin/bash
 #SBATCH --nodes=1
@@ -26,15 +27,15 @@ module load python/anaconda3
 source activate dsa
 poetry install
 
-(poetry run python -m src.dsa_optimization --n_delay {n_delay} --delay_interval {delay_interval} --ordered {ordered}) &
+(poetry run python -m src.dsa_optimization --n_delay {n_delay} --delay_interval {delay_interval} --no-ordered) &
 
 # PID of the application
 APP_PID=$!
 
 # Monitor GPU status every 300 seconds (5 minutes) until the application finishes
 while kill -0 $APP_PID 2>/dev/null; do
-    echo "$(date '+%Y-%m-%d %H:%M:%S') - Checking GPU status during the application run:" >> gpu_usage/{n_delay}_{delay_interval}_{ordered}_gpu_usage.log
-    nvidia-smi >> gpu_usage/{n_delay}_{delay_interval}_{ordered}_gpu_usage.log  # Append output to log file
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Checking GPU status during the application run:" >> gpu_usage/{n_delay}_{delay_interval}_noordered_gpu_usage.log
+    nvidia-smi >> gpu_usage/{n_delay}_{delay_interval}_noordered_gpu_usage.log  # Append output to log file
     sleep 300 
 done
 
@@ -51,17 +52,16 @@ wait $APP_PID
     for delay in n_delays[::-1]:
         for space in delay_interval[::-1]:
             if space < int(200 / delay):
-                for ordered in [False]:
-                    script_content = script_template.format(
-                        n_delay=delay, delay_interval=space, ordered=ordered
-                    )
-                    script_filename = f"sbatch/dsa/{delay}_{space}_{ordered}_script.sh"
+                script_content = script_template.format(
+                    n_delay=delay, delay_interval=space
+                )
+                script_filename = f"sbatch/dsa/{delay}_{space}_noordered_script.sh"
 
-                    with open(script_filename, "w") as script_file:
-                        script_file.write(script_content)
+                with open(script_filename, "w") as script_file:
+                    script_file.write(script_content)
 
-                    # Submit the job to the cluster
-                    call(f"sbatch {script_filename}", shell=True)
+                # Submit the job to the cluster
+                call(f"sbatch {script_filename}", shell=True)
 
 
 if __name__ == "__main__":
