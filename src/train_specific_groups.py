b3c30f2c70125d7a2958bd7ca75b2bdbb7167302
qglht <quentin.guilhot@student-cs.fr>
2024-07-05 14:48:41 +0100

    all

---
+++ src/train_specific_groups.py
@@ -0,0 +1,74 @@
+import numpy as np
+import os
+import sys
+from subprocess import call
+import warnings
+import argparse
+from dsa_analysis import load_config
+import torch
+import multiprocessing
+from src.toolkit import pipeline
+from src.train_group import train
+
+
+def generate_and_submit_scripts(args: argparse.Namespace):
+    config = load_config("config.yaml")
+
+    script_template = """#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --job-name={taskset}_{group}_job
+#SBATCH --gres=gpu:8
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=80  
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=oxfd2547@ox.ac.uk
+
+module load cuda/11.2
+module load pytorch/1.9.0
+module load python/anaconda3
+
+source activate dsa
+poetry install
+
+(poetry run python -m src.train_group --taskset {taskset} --group {group}) & 
+
+# PID of the application
+APP_PID=$!
+
+# Monitor GPU status every 300 seconds (5 minutes) until the application finishes
+while kill -0 $APP_PID 2>/dev/null; do
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - Checking GPU status during the application run:" >> gpu_usage/{taskset}_{group}_gpu_usage.log
+    nvidia-smi >> gpu_usage/{taskset}_{group}_gpu_usage.log  # Append output to log file
+    sleep 300 
+done
+
+wait $APP_PID
+"""
+    groups = [
+        "pretrained_basic_anti_frozen",
+        "pretrained_basic_anti_unfrozen",
+        "pretrained_basic_delay_frozen",
+        "pretrained_basic_delay_unfrozen",
+    ]
+    for group in groups:
+        script_content = script_template.format(taskset=args.taskset, group=group)
+        script_filename = f"sbatch/groups/{args.taskset}_{group}_script.sh"
+
+        with open(script_filename, "w") as script_file:
+            script_file.write(script_content)
+
+        # Submit the job to the cluster
+        call(f"sbatch {script_filename}", shell=True)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train the model")
+    parser.add_argument(
+        "--taskset",
+        type=str,
+        default="PDM",
+        help="The taskset to train the model on",
+    )
+    args = parser.parse_args()
+    generate_and_submit_scripts(args)
