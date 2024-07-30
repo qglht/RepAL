02694de74706c78b7f79e93453c7350b6b21392f
qglht <quentin.guilhot@student-cs.fr>
2024-07-30 12:41:06 +0100

    src

--- src/dissimilarity.py
+++ src/dissimilarity.py
@@ -14,7 +14,7 @@ from src.dsa_optimization import dsa_computation
 def generate_and_submit_scripts(args: argparse.Namespace):
     script_template = """#!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --time=2:00:00
+#SBATCH --time=0:45:00
 #SBATCH --job-name={taskset}_{group1}_VS_{group2}_job
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks-per-node=1
